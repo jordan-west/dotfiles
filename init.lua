267bad04138fb60594ccae6706fb12f945c1e755
Jordan West <jordan.liam.west@gmail.com>
2023-08-02 19:03:18 +0100

    cmp config

--- init.lua
+++ init.lua
@@ -56,8 +56,9 @@ require("packer").startup(function(use)
     use({ "nvim-telescope/telescope.nvim", tag = "0.1.1" })
     -- LSP
     use("neovim/nvim-lspconfig")
-    use("hrsh7th/nvim-cmp")
     use("hrsh7th/cmp-nvim-lsp")
+    use("hrsh7th/cmp-buffer")
+    use("hrsh7th/nvim-cmp")
 end)
 
 -- Colorscheme
@@ -78,6 +79,7 @@ vim.keymap.set("n", "<leader>ff", function() builtin.find_files({ follow = true 
 vim.keymap.set("n", "<leader>fg", function() builtin.live_grep({ additional_args = { "--follow" } }) end, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
 
 -- LSP
 local on_attach = function(client, bufnr)
@@ -92,7 +94,28 @@ local on_attach = function(client, bufnr)
     vim.diagnostic.disable()
 end
 
+local on_attach_rust = function(client, bufnr)
+    local opts = { buffer = bufnr }
+
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "gs", builtin.lsp_document_symbols, opts)
+end
+
+local on_attach_go = function(client, bufnr)
+    local opts = { buffer = bufnr }
+
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "gs", builtin.lsp_document_symbols, opts)
+end
+
 local cmp = require("cmp")
+local types = require("cmp.types")
 
 cmp.setup({
     sources = {
@@ -102,13 +125,64 @@ cmp.setup({
     completion = {
         autocomplete = false,
     },
+    mapping = {
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<Up>"] = cmp.mapping.select_prev_item(),
+        ["<Down>"] = cmp.mapping.select_next_item(),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-3),
+        ["<C-f>"] = cmp.mapping.scroll_docs(3),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm(),
+    },
+})
+
+cmp.setup.filetype("go", {
+    completion = {
+        autocomplete = {
+            types.cmp.TriggerEvent.TextChanged,
+        },
+    },
 })
 
 vim.keymap.set("i", "<C-x><C-o>", cmp.complete, {})
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local lspconfig = require("lspconfig")
+local util = require("lspconfig/util")
 
-require("lspconfig").clangd.setup({
+capabilities.textDocument.completion.completionItem.snippetSupport = false
+
+lspconfig.clangd.setup({
     on_attach = on_attach,
+    capabilities = capabilities,
+})
+
+lspconfig.lua_ls.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
+
+lspconfig.pyright.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
+
+lspconfig.rust_analyzer.setup({
+    on_attach = on_attach_rust,
+    capabilities = capabilities,
+})
+
+lspconfig.gopls.setup({
+    cmd = {"gopls", "serve"},
+    filetypes = {"go", "gomod"},
+    root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+    settings = {
+        gopls = {
+            staticcheck = true,
+        },
+    },
+    on_attach = on_attach_go,
     capabilities = capabilities,
 })
